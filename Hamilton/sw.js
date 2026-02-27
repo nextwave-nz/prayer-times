8c38d0ff137b8d6598b55a8220c019f2febf972d
nextwave-nz <hello@nextwave.nz>
2026-02-28 04:31:28 +1300

    Update sw.js

--- Hamilton/sw.js
+++ Hamilton/sw.js
@@ -1,24 +1,15 @@
-const CACHE_NAME = 'hamilton-masjid-v11';
-const urlsToCache = ['./', './index.html', './manifest.json'];
-
-self.addEventListener('install', (event) => {
-    event.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(urlsToCache)));
-    self.skipWaiting();
+var CACHE_NAME = 'hamilton-masjid-v12';
+var urlsToCache = ['./', './manifest.json', './icon-192x192.png', './icon-512x512.png'];
+self.addEventListener('install', function(e) {
+  e.waitUntil(caches.open(CACHE_NAME).then(function(c) { return c.addAll(urlsToCache) }));
+  self.skipWaiting();
 });
-
-self.addEventListener('fetch', (event) => {
-    event.respondWith(
-        fetch(event.request).then((response) => {
-            const clone = response.clone();
-            caches.open(CACHE_NAME).then((cache) => { cache.put(event.request, clone); });
-            return response;
-        }).catch(() => caches.match(event.request))
-    );
+self.addEventListener('activate', function(e) {
+  e.waitUntil(caches.keys().then(function(n) {
+    return Promise.all(n.filter(function(k) { return k !== CACHE_NAME }).map(function(k) { return caches.delete(k) }));
+  }));
+  self.clients.claim();
 });
-
-self.addEventListener('activate', (event) => {
-    event.waitUntil(
-        caches.keys().then((names) => Promise.all(names.map((n) => n !== CACHE_NAME ? caches.delete(n) : undefined)))
-    );
-    self.clients.claim();
+self.addEventListener('fetch', function(e) {
+  e.respondWith(fetch(e.request).catch(function() { return caches.match(e.request) }));
 });
